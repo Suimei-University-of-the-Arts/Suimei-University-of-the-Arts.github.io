689080ac0f353951c4d8aa175df5c9723a3a4684
Dai Longao <dai@jiong3.cn>
2020-07-25 23:37:37 +0800

    new site initial commit

---
+++ js/data.js
@@ -0,0 +1,160 @@
+let problems = [
+  {
+    name: '2019 陕西省大学生程序设计竞赛',
+    ranklist: 'http://acm.xidian.edu.cn/board-2019/',
+    website: null,
+    probNum: 12,
+    pdf: 'http://acm.zju.edu.cn/contest-materials/sxp2019/sxp2019_problems.pdf',
+    practice: 'http://acm.zju.edu.cn/onlinejudge/searchProblem.do?contestId=1&titlefrom=0&authorfrom=0&sourcefrom=0&query=The+2019+ICPC+China+Shaanxi+Provincial+Programming+Contest',
+    probOffAc: [0, 57, 13, 2, 75, 92, 0, 5, 11, 9, 1, 94],
+    probUnoffAc: [2, 4, 2, 2, 5, 6, 1, 0, 2, 2, 2, 6],
+    teamOffAc: [9, 5, 15, 21, 36, 8, 5, 2, 3, 1, 0, 0, 0],
+    teamUnoffAc: [0, 0, 1, 1, 2, 0, 0, 0, 0, 0, 1, 1, 0]
+  },
+  {
+    name: '2019 山东省大学生程序设计竞赛',
+    ranklist: 'http://acm.zju.edu.cn/contest-materials/sdp2019/board/',
+    website: 'http://acm.ujn.edu.cn/icpc/',
+    probNum: 13,
+    pdf: 'http://acm.zju.edu.cn/contest-materials/sdp2019/sdp2019_problems.pdf',
+    practice: 'http://acm.zju.edu.cn/onlinejudge/searchProblem.do?contestId=1&titlefrom=0&authorfrom=0&sourcefrom=0&query=The+10th+Shandong+Provincial+Collegiate+Programming+Contest',
+    probOffAc: [241, 29, 171, 189, 13, 230, 1, 46, 1, 2, 15, 48, 238],
+    probUnoffAc: [56, 5, 40, 44, 3, 53, 1, 9, 0, 0, 6, 13, 53],
+    teamOffAc: [2, 11, 6, 40, 24, 94, 30, 18, 16, 2, 3, 3, 0, 0],
+    teamUnoffAc: [2, 2, 1, 8, 7, 21, 8, 2, 4, 2, 1, 0, 0, 0]
+  },
+  {
+    name: '2019 浙江省大学生程序设计竞赛',
+    ranklist: 'http://acm.zju.edu.cn/zjp2019/board/',
+    website: 'http://acm.zju.edu.cn/zjp2019/',
+    probNum: 13,
+    pdf: 'http://acm.zju.edu.cn/zjp2019/zjp2019_problems.pdf',
+    practice: 'http://acm.zju.edu.cn/onlinejudge/searchProblem.do?contestId=1&titlefrom=0&authorfrom=0&sourcefrom=0&query=The+16th+Zhejiang+Provincial+Collegiate+Programming+Contest+Sponsored+by+TuSimple',
+    probOffAc: [5, 70, 22, 11, 186, 280, 286, 221, 242, 82, 59, 0, 0],
+    probUnoffAc: [7, 11, 10, 8, 11, 11, 11, 11, 11, 11, 10, 0, 2],
+    teamOffAc: [0, 6, 35, 21, 44, 84, 25, 24, 21, 20, 3, 3, 0, 0],
+    teamUnoffAc: [0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 0, 5, 2, 0]
+  },
+  {
+    name: '2019 浙江大学程序设计竞赛',
+    ranklist: 'http://acm.zju.edu.cn/contest-materials/zju2019/board/',
+    website: null,
+    probNum: 10,
+    pdf: 'http://acm.zju.edu.cn/contest-materials/zju2019/zju2019_problems.pdf',
+    practice: 'http://acm.zju.edu.cn/onlinejudge/searchProblem.do?contestId=1&titlefrom=0&authorfrom=0&sourcefrom=0&query=The+19th+Zhejiang+University+Programming+Contest+Sponsored+by+TuSimple',
+    probOffAc: [70, 52, 42, 7, 133, 0, 69, 8, 4, 114],
+    probUnoffAc: [79, 61, 50, 16, 93, 10, 86, 25, 23, 92],
+    teamOffAc: [11, 16, 42, 15, 12, 13, 24, 9, 2, 2, 0],
+    teamUnoffAc: [2, 3, 3, 10, 19, 11, 21, 6, 9, 5, 8]
+  },
+  {
+    name: '2018 ACM-ICPC 亚洲区域赛青岛站',
+    ranklist: 'http://acm.upc.edu.cn/rank/',
+    website: 'http://acm.upc.edu.cn',
+    probNum: 13,
+    pdf: 'http://acm.zju.edu.cn/contest-materials/qd2018/qd2018_problems.pdf',
+    practice: 'http://acm.zju.edu.cn/onlinejudge/searchProblem.do?contestId=1&titlefrom=0&authorfrom=0&sourcefrom=0&query=The+2018+ACM-ICPC+Asia+Qingdao+Regional+Contest',
+    probOffAc: [0, 0, 333, 88, 109, 105, 5, 0, 2, 332, 7, 17, 353],
+    probUnoffAc: [0, 2, 18, 11, 12, 9, 3, 0, 4, 18, 3, 6, 19],
+    teamOffAc: [3, 9, 23, 146, 83, 50, 28, 7, 4, 3, 0, 0, 0, 0],
+    teamUnoffAc: [0, 1, 0, 5, 2, 2, 4, 0, 2, 1, 0, 2, 0, 0]
+  },
+  {
+    name: '2018 ACM-ICPC 亚洲区域赛青岛站网络赛',
+    ranklist: 'http://acm.zju.edu.cn/contest-materials/qdol2018/board/',
+    website: 'http://acm.upc.edu.cn',
+    probNum: 11,
+    pdf: 'http://acm.zju.edu.cn/contest-materials/qdol2018/qdol2018_problems.pdf',
+    practice: 'http://acm.zju.edu.cn/onlinejudge/searchProblem.do?contestId=1&titlefrom=0&authorfrom=0&sourcefrom=0&query=The+2018+ACM-ICPC+Asia+Qingdao+Regional+Contest%2C+Online',
+    probAc: [1492, 147, 971, 22, 0, 38, 121, 933, 7, 576, 1359],
+    teamAc: [0, 204, 311, 178, 306, 368, 93, 53, 24, 9, 4, 0]
+  },
+  {
+    name: '2018 浙江省大学生程序设计竞赛',
+    ranklist: 'http://acm.zju.edu.cn/zjp2018/board/',
+    website: 'http://acm.zju.edu.cn/zjp2018/',
+    probNum: 13,
+    pdf: 'http://acm.zju.edu.cn/zjp2018/zjp2018_problems.pdf',
+    practice: 'http://acm.zju.edu.cn/onlinejudge/searchProblem.do?contestId=1&titlefrom=0&authorfrom=0&sourcefrom=0&query=The+15th+Zhejiang+Provincial+Collegiate+Programming+Contest+Sponsored+by+TuSimple',
+    probOffAc: [260, 245, 9, 48, 18, 44, 2, 3, 12, 185, 95, 209, 277],
+    probUnoffAc: [7, 7, 7, 7, 7, 7, 2, 3, 5, 7, 7, 7, 7],
+    teamOffAc: [2, 12, 22, 27, 40, 71, 50, 23, 12, 12, 2, 3, 2, 1],
+    teamUnoffAc: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 1, 2]
+  },
+  {
+    name: '2018 浙江大学程序设计竞赛',
+    ranklist: 'http://acm.zju.edu.cn/contest-materials/zju2018/board/',
+    website: null,
+    probNum: 10,
+    pdf: 'http://acm.zju.edu.cn/contest-materials/zju2018/zju2018_problems.pdf',
+    practice: 'http://acm.zju.edu.cn/onlinejudge/searchProblem.do?contestId=1&titlefrom=0&authorfrom=0&sourcefrom=0&query=The+18th+Zhejiang+University+Programming+Contest+Sponsored+by+TuSimple',
+    probOffAc: [134, 0, 56, 8, 0, 25, 55, 6, 6, 135],
+    probUnoffAc: [42, 0, 26, 9, 0, 20, 31, 8, 5, 42],
+    teamOffAc: [0, 1, 67, 22, 18, 16, 7, 2, 2, 0, 0],
+    teamUnoffAc: [0, 0, 9, 7, 8, 5, 7, 3, 3, 0, 0],
+  },
+  {
+    name: '2017 中国大学生程序设计竞赛秦皇岛站',
+    ranklist: 'http://acm.zju.edu.cn/contest-materials/qhd2017/board/',
+    website: null,
+    probNum: 13,
+    pdf: 'http://acm.zju.edu.cn/contest-materials/qhd2017/qhd2017_problems.pdf',
+    practice: 'http://acm.zju.edu.cn/onlinejudge/searchProblem.do?contestId=1&titlefrom=0&authorfrom=0&sourcefrom=0&query=The+2017+China+Collegiate+Programming+Contest%2C+Qinhuangdao+Site',
+    probAc: [87, 4, 234, 7, 221, 0, 75, 26, 0, 0, 1, 247, 196],
+    teamAc: [5, 10, 17, 29, 90, 40, 38, 16, 5, 3, 0, 0, 0, 0]
+  },
+  {
+    name: '2017 浙江省大学生程序设计竞赛',
+    ranklist: 'http://acm.zju.edu.cn/zjp2017/board/',
+    website: 'http://acm.zju.edu.cn/zjp2017/',
+    probNum: 13,
+    pdf: 'http://acm.zju.edu.cn/zjp2017/zjp2017_problems.pdf',
+    practice: 'http://acm.zju.edu.cn/onlinejudge/searchProblem.do?contestId=1&titlefrom=0&authorfrom=0&sourcefrom=0&query=The+14th+Zhejiang+Provincial+Collegiate+Programming+Contest+Sponsored+by+TuSimple',
+    probOffAc: [285, 259, 183, 180, 47, 28, 6, 14, 1, 0, 0, 0, 0],
+    probUnoffAc: [8, 8, 8, 8, 8, 8, 5, 5, 0, 0, 0, 0, 0],
+    teamOffAc: [2, 25, 61, 36, 110, 29, 12, 6, 5, 1, 0, 0, 0, 0],
+    teamUnoffAc: [0, 0, 0, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, 0],
+  },
+  {
+    name: '2017 浙江大学程序设计竞赛',
+    ranklist: 'http://acm.zju.edu.cn/contest-materials/zju2017/board/',
+    website: null,
+    probNum: 10,
+    pdf: 'http://acm.zju.edu.cn/contest-materials/zju2017/zju2017_problems.pdf',
+    practice: 'http://acm.zju.edu.cn/onlinejudge/searchProblem.do?contestId=1&titlefrom=0&authorfrom=0&sourcefrom=0&query=The+17th+Zhejiang+University+Programming+Contest+Sponsored+by+TuSimple',
+    probOffAc: [120, 4, 57, 1, 8, 17, 46, 14, 12, 129],
+    probUnoffAc: [28, 8, 22, 2, 1, 11, 17, 12, 13, 28],
+    teamOffAc: [4, 9, 55, 26, 21, 8, 4, 3, 3, 2, 0],
+    teamUnoffAc: [0, 2, 5, 3, 4, 3, 3, 2, 6, 0, 1]
+  }
+];
+
+/*
+const PROB_NUM = 10;
+const DELTA = 2;
+
+let prob_ac = [];
+let team_ac = [];
+
+for (let i = 0; i < PROB_NUM; i++) {
+  prob_ac.push([0, 0]);
+}
+for (let i = 0; i <= PROB_NUM; i++) {
+  team_ac.push([0, 0]);
+}
+
+let teams = [document.querySelectorAll('.official'), document.querySelectorAll('.unofficial')];
+
+for (let k = 0; k <= 1; k++) {
+  for (let i = 0; i < teams[k].length; i++) {
+    let team = teams[k][i];
+    team_ac[parseInt(team.children[DELTA].innerHTML)][k]++;
+    for (let j = 0; j < PROB_NUM; j++) {
+      let ch = team.children[j + DELTA + 2];
+      if (ch.classList.contains('accepted') || ch.classList.contains('firstblood')) {
+        prob_ac[j][k]++;
+      }
+    }
+  }
+}
+*/
